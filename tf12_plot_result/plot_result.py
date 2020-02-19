ffb450904c3ad26340688385bd599af105e1d4fc
thanyoung <1247762780@qq.com>
2020-02-19 16:46:15 +0800

    update 2020-02-19

---
+++ tf12_plot_result/plot_result.py
@@ -0,0 +1,71 @@
+# 2020-02-18
+
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import matplotlib.plotpy as plt
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# make up real data
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.squre(x_data) - 0.5 + noise
+
+plt.scatter(x_data, y_data)
+plt.show()
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# the error between prediction and real data
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# plot the real data
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(1, 1001):
+        # training
+        sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+        if i % 50 == 0:
+            # to visualize the result and improvement
+            try:
+                ax.lines.remove(lines[0])
+            except:
+                pass
+            prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+            # plot the prediction
+            lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+            plt.pause(1)
+
